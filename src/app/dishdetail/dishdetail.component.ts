fa269bb46333e9007edfd36c6a1f7b6f547e57e2
nazeersathak <nazeersathak@gmail.com>
2022-08-30 12:51:40 +0530

    animations part-2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,7 +5,7 @@ import {Params,ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 import {switchMap} from 'rxjs/operators';
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
-import {trigger,state,style,animate,transition} from '@angular/animations'
+import { flyInOut, visibility,expand } from '../animations/app.animation';
 
  
 
@@ -13,19 +13,7 @@ import {trigger,state,style,animate,transition} from '@angular/animations'
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
-  animations:[
-    trigger('visibility',[
-      state('shown',style({
-        transform:'scale(1.0',
-        opacity:1
-      })),
-      state('hidden',style({
-        transform:'scale(0.5)',
-        opacity:0
-      })),
-      transition('* => *',animate('0.5s ease-in-out'))
-    ])
-  ]
+  animations:[ visibility(),flyInOut(),expand()]
 })
 export class DishdetailComponent implements OnInit {
   @ViewChild('fform') commentFormDirective;
